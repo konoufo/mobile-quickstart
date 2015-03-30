e5023832517faba31ee47af9405cdd4669a8768d
rchance3333 <richard@hotcoffeydesign.com>
2015-03-30 10:09:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def token():
      capability.allow_client_outgoing(app_sid)
 
   # This allows incoming connections to client (if specified)
-  client = request.values.get('mobile-quickstat1')
+  client = request.values.get('client')
   if client != None:
     capability.allow_client_incoming("mobile-quickstart1")
 
